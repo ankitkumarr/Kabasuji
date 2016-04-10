7fa3efb682b2d57ce912c896063a19a2ed76aa6b
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-04-10 15:23:04 -0400

    Add minor changes to Main and Application on Player Side

--- src/com/halaesus/kabasuji/player/boundary/Application.java
+++ src/com/halaesus/kabasuji/player/boundary/Application.java
@@ -1,11 +1,7 @@
 package com.halaesus.kabasuji.player.boundary;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
 import com.halaesus.kabasuji.player.entity.Model;
 
@@ -18,7 +14,8 @@ public class Application extends JFrame {
 	LevelSelectorView levelSelectorView;
 
 	public Application(Model masterModel) {
-		setTitle("Halaesus Kabasuji");
+		// Basic GUI Stuff
+		setTitle("Kabasuji by Team Halaesus");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1280, 720);
 		setResizable(false); // Cannot rescale the window
@@ -36,13 +33,11 @@ public class Application extends JFrame {
 	public void showSplashScreen() {
 		currentView = splashView; // Our current view is the Splash Screen
 		setContentPane(currentView); // Show it on the UI
-//		setUndecorated(true); // Get rid of the window border
 	}
 
 	public void showLevelSelector() {
 		currentView = levelSelectorView;
 		setContentPane(currentView);
-//		setUndecorated(false);
 		repaint();
 	}
 

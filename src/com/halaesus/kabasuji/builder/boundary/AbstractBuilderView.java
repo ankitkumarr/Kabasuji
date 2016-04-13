893323a7b942c1073c4c681bd27e73d5230637e3
Corey Dixon <zimhey1@gmail.com>
2016-04-13 01:25:21 -0400

    Replaced Images with JButtons as per the analysis

--- src/com/halaesus/kabasuji/builder/boundary/AbstractBuilderView.java
+++ src/com/halaesus/kabasuji/builder/boundary/AbstractBuilderView.java
@@ -1,11 +1,13 @@
 package com.halaesus.kabasuji.builder.boundary;
 
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -14,74 +16,114 @@ import javax.swing.JPanel;
 import com.halaesus.kabasuji.builder.entity.SplashModel;
 
 @SuppressWarnings("serial")
-public class AbstractBuilderView extends JPanel { // you'll note this isn't actually an abstract class
-	
-    JButton saveBtn;
-    JButton openBtn;
-    JButton undoBtn;
-    JButton redoBtn;
-    JButton flipH;
-    JButton flipV;
-    JButton rotateCC;
-    JButton rotateCW;
-    JButton newBtn;
+public class AbstractBuilderView extends JPanel { // you'll note this isn't
+													// actually an abstract
+													// class
+	JButton newBtn;
+	JButton saveBtn;
+	JButton openBtn;
+	JButton undoBtn;
+	JButton redoBtn;
+	JButton flipHBtn;
+	JButton flipVBtn;
+	JButton rotateCCBtn;
+	JButton rotateCWBtn;
 
-    JLabel levelInfo;
-    JLabel bullpenLevel;
-    JLabel[] playerHexsCount;
+	JLabel levelInfo;
+	JLabel bullpenLevel;
+	JLabel[] playerHexsCount;
 
-    BufferedImage[] workspacePiece;
-    BufferedImage[] bullpenPaletteHexs;
-    BufferedImage[] playerPaletteHexs;
-    BufferedImage[] boardSquares;
-    BufferedImage[] boardPieceSquares;
-    
-    public AbstractBuilderView() {
+	BufferedImage[] workspacePiece;
+	JButton[] hexominoImages;
+	BufferedImage[] boardSquares;
+	BufferedImage[] boardPieceSquares;
+
+
+	public AbstractBuilderView() {
 		// Set GUI Bounds
 		setBounds(0, 0, 1280, 720);
 		// Set up LayoutManager to null
 		setLayout(null);
 		// Set up the popup that handles selecting the level type
+		try {
+			newBtn = new JButton("New");
+			newBtn.setFont(new Font("Arial", Font.PLAIN, 14));
+			newBtn.setBounds(10, 15, 60, 60);
+			this.add(newBtn);
+			saveBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/save.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			saveBtn.setBounds(10 + 60 * 2, 15, 60, 60);
+			this.add(saveBtn);
+			openBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/open.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			openBtn.setBounds(10 + 60 * 1, 15, 60, 60);
+			this.add(openBtn);
+			undoBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/undo.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			undoBtn.setBounds(10 + 60 * 3, 15, 60, 60);
+			this.add(undoBtn);
+			redoBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/redo.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			redoBtn.setBounds(10 + 60 * 4, 15, 60, 60);
+			this.add(redoBtn);
+			
+			flipHBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/flipHorizontal.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			flipHBtn.setBounds(230, 615, 60, 60);
+			this.add(flipHBtn);
+			
+			flipVBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/flipVertical.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			flipVBtn.setBounds(10, 395, 60, 60);
+			this.add(flipVBtn);
+			
+			rotateCWBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/rotateCW.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			rotateCWBtn.setBounds(10, 615, 60, 60);
+			this.add(rotateCWBtn);
+			
+			rotateCCBtn = new JButton((new ImageIcon(ImageIO.read(getClass().getResource("/resources/rotateCC.png")).getScaledInstance(60, 60,
+					Image.SCALE_SMOOTH))));
+			rotateCCBtn.setBounds(230, 395, 60, 60);	
+			this.add(rotateCCBtn);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	public void showDialog(Application app) {
+		// options in drop down menu
+		String[] choices = { "Open from file", "New Puzzle Level", "New Lightning Level", "New Release Level" };
 
-    public void showDialog(Application app) {
-    	// options in drop down menu
-		String[] choices = {"Open from file", "New Puzzle Level",
-						"New Lightning Level", "New Release Level"};
-		
 		// create popup, store response as String
-		String action = (String) JOptionPane.showInputDialog(app,
-				"Open or create a new level", // text within popup
+		String action = (String) JOptionPane.showInputDialog(app, "Open or create a new level", // text
+																								// within
+																								// popup
 				"Select an option", // window title
 				JOptionPane.QUESTION_MESSAGE, // icon
-				null,
-				choices,
-				choices[0]);
-		if (action == null){ // no input
+				null, choices, choices[0]);
+		if (action == null) { // no input
 			// TODO
 			return;
-		}
-		else if (action.equals(choices[0])){ // open from file
+		} else if (action.equals(choices[0])) { // open from file
 			// TODO
 			app.showPuzzleBuilderView();
 			return;
-		}
-		else if (action.equals(choices[1])){ // new puzzle
+		} else if (action.equals(choices[1])) { // new puzzle
 			app.showPuzzleBuilderView();
 			return;
-		}
-		else if (action.equals(choices[2])){ // new lightning
+		} else if (action.equals(choices[2])) { // new lightning
 			app.showLightningBuilderView();
 			return;
-		}
-		else if (action.equals(choices[3])){ // new release
+		} else if (action.equals(choices[3])) { // new release
 			app.showReleaseBuilderView();
 			return;
 		}
-    }
-    
-    protected void paintComponent(Graphics g) {
+	}
+
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g); // Let the JPanel do its stuff
 		// Render the background image
 		try {
@@ -90,69 +132,13 @@ public class AbstractBuilderView extends JPanel { // you'll note this isn't actu
 		} catch (IOException e) {
 			// Don't render the background
 		}
-
-		drawNewButton(g);
-		drawOpenButton(g);
-		drawSaveButton(g);
-		drawUndoButton(g);
-		drawRedoButton(g);
 		drawWorkspace(g);
 
 		drawPlayerPalette(g);
 		drawBuilderPalette(g);
 		drawBoard(g);
 	}
-
-	private void drawNewButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/backButton.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 10, 15, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
-	private void drawOpenButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/backButton.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 10 + 60, 15, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
-	private void drawSaveButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/backButton.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 10 + 60 * 2, 15, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
-	private void drawUndoButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/backButton.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 10 + 60 * 3, 15, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
-	private void drawRedoButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/backButton.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 10 + 60 * 4, 15, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
+	
 	private void drawWorkspace(Graphics g) {
 		// Load up the left panel image
 		try {
@@ -162,57 +148,30 @@ public class AbstractBuilderView extends JPanel { // you'll note this isn't actu
 			// right border
 			g.drawImage(ImageIO.read(getClass().getResource("/resources/bullpenWindow.jpg")).getScaledInstance(-1, 612,
 					Image.SCALE_SMOOTH), 968, 80, null);
-			// workspace background
+			// builder palette background
 			g.drawImage(ImageIO.read(getClass().getResource("/resources/paletteWindow.jpg")).getScaledInstance(288, -1,
-					Image.SCALE_SMOOTH), 9, 394, null);
+					Image.SCALE_SMOOTH), 9, 85, null);
+			// player palette background
+			g.drawImage(ImageIO.read(getClass().getResource("/resources/paletteWindow.jpg")).getScaledInstance(288, -1,
+					Image.SCALE_SMOOTH), 977, 395, null);
 		} catch (IOException ex) {
 			return; // Cannot do anything. We tried :(
 		}
 		drawWorkspacePiece(g);
-		drawRotateCWButton(g);
-		drawRotateCCButton(g);
-		drawFlipHButton(g);
-		drawFlipVButton(g);
 	}
 
 	private void drawWorkspacePiece(Graphics g) {
 		// TODO drawWorkspacePiece()
 	}
 
-	private void drawRotateCWButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/rotateCW.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 10, 615, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
-	private void drawRotateCCButton(Graphics g) {
-		// Load up the image
-		try {
-			g.drawImage(ImageIO.read(getClass().getResource("/resources/rotateCC.png")).getScaledInstance(60, 60,
-					Image.SCALE_SMOOTH), 230, 395, null);
-		} catch (IOException ex) {
-			return; // Cannot do anything. We tried :(
-		}
-	}
-
-	private void drawFlipHButton(Graphics g) {
-		// TODO drawFlipHButton()
-	}
-
-	private void drawFlipVButton(Graphics g) {
-		// TODO drawFlipVButton()
-	}
 
 	private void drawPlayerPalette(Graphics g) {
 		// TODO drawPlayerPalette()
+		
 	}
 
 	private void drawBuilderPalette(Graphics g) {
-
+		// TODO drawBuilderPalette()
 	}
 
 	private void drawBoard(Graphics g) {

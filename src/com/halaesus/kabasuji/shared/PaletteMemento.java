bfed6fec561a377c597f5eb2721f5b424d63a012
Corey Dixon <Corey Dixon>
2016-04-26 04:14:23 -0400

    Added PaletteMemento stub

---
+++ src/com/halaesus/kabasuji/shared/PaletteMemento.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.halaesus.kabasuji.shared;
+
+/**
+ * @author Corey Dixon
+ *
+ */
+public class PaletteMemento {
+
+	public PaletteMemento() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

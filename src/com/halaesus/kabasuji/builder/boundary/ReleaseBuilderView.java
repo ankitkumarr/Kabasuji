1182c1113e39c580419befa366d5934286dd3066
Ankit Kumar <kumar.ankit55@gmail.com>
2016-04-30 00:00:44 -0400

    Added the numbers in the releaseView of builder

--- src/com/halaesus/kabasuji/builder/boundary/ReleaseBuilderView.java
+++ src/com/halaesus/kabasuji/builder/boundary/ReleaseBuilderView.java
@@ -1,17 +1,52 @@
 package com.halaesus.kabasuji.builder.boundary;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import com.halaesus.kabasuji.shared.entity.AbstractLevel;
 import com.halaesus.kabasuji.shared.entity.ReleaseLevel;
+import com.halaesus.kabasuji.shared.entity.ReleaseNumber;
 
 @SuppressWarnings("serial")
 public class ReleaseBuilderView extends AbstractBuilderView {
-    BufferedImage[] numberBarNumbers;
+    JLabel[] numberBarNumbers;
     ReleaseLevel level;
+    ReleaseNumber rNumbers[][];
 
     public ReleaseBuilderView(Application application, AbstractLevel aLevel) {
-        // TODO implement here
+        
     	super(application, aLevel);
+    	this.level = (ReleaseLevel) aLevel;
+    	
+    	setupReleasePalette();
+    }
+    
+    public void setupReleasePalette() {
+    	Font releaseNumberFont = new Font("releaseNumberFont", Font.BOLD, 35);
+    	rNumbers = new ReleaseNumber[3][6];
+    	
+    	for (int j = 1; j <= 3; j++) {
+			for (int i = 1; i <= 6; i++) {
+				ReleaseNumber rn = new ReleaseNumber(i, j, 0 , 0);
+				rNumbers[j-1][i-1] = rn; //956
+				JLabel n = new JLabel(Integer.toString(rn.getValue()));
+				n.setHorizontalAlignment(SwingConstants.CENTER);
+				n.setBounds(960 + (53*(i-1)) , 80 + (53*(j-1)) , 53, 53);
+				if (rn.getColor() == 1)n.setForeground(Color.RED);
+				if (rn.getColor() == 2)n.setForeground(Color.YELLOW);
+				if (rn.getColor() == 3)n.setForeground(Color.CYAN);
+				n.setFont(releaseNumberFont);
+				n.setVisible(true);
+				add(n);
+			}
+		}
+    	
+    	this.repaint();
+    	
+    	
     }
 }

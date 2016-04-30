5b97a070f3fa2d1aabb55d75658cfa48e013e5c2
Ankit Kumar <kumar.ankit55@gmail.com>
2016-04-30 00:28:35 -0400

    Small change in the NumberBar for the builder

--- src/com/halaesus/kabasuji/builder/boundary/ReleaseBuilderView.java
+++ src/com/halaesus/kabasuji/builder/boundary/ReleaseBuilderView.java
@@ -7,6 +7,7 @@ import java.awt.image.BufferedImage;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
+import com.halaesus.kabasuji.builder.controller.DragNumberToBoardMove;
 import com.halaesus.kabasuji.shared.entity.AbstractLevel;
 import com.halaesus.kabasuji.shared.entity.ReleaseLevel;
 import com.halaesus.kabasuji.shared.entity.ReleaseNumber;
@@ -27,15 +28,18 @@ public class ReleaseBuilderView extends AbstractBuilderView {
     
     public void setupReleasePalette() {
     	Font releaseNumberFont = new Font("releaseNumberFont", Font.BOLD, 35);
-    	rNumbers = new ReleaseNumber[3][6];
+    	rNumbers = this.level.getNumberBar().getNumbers();
+    	//rNumbers = new ReleaseNumber[3][6];
     	
     	for (int j = 1; j <= 3; j++) {
 			for (int i = 1; i <= 6; i++) {
-				ReleaseNumber rn = new ReleaseNumber(i, j, 0 , 0);
-				rNumbers[j-1][i-1] = rn; //956
+				//ReleaseNumber rn = new ReleaseNumber(i, j, 0 , 0);
+				ReleaseNumber rn = rNumbers[j-1][i-1];
+				//rNumbers[j-1][i-1] = rn; //956
 				JLabel n = new JLabel(Integer.toString(rn.getValue()));
 				n.setHorizontalAlignment(SwingConstants.CENTER);
 				n.setBounds(960 + (53*(i-1)) , 80 + (53*(j-1)) , 53, 53);
+				n.addMouseListener(new DragNumberToBoardMove(this.level, this));
 				if (rn.getColor() == 1)n.setForeground(Color.RED);
 				if (rn.getColor() == 2)n.setForeground(Color.YELLOW);
 				if (rn.getColor() == 3)n.setForeground(Color.CYAN);
--- src/com/halaesus/kabasuji/builder/controller/DragNumberToBoardMove.java
+++ src/com/halaesus/kabasuji/builder/controller/DragNumberToBoardMove.java
@@ -1,25 +1,28 @@
 package com.halaesus.kabasuji.builder.controller;
 
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import com.halaesus.kabasuji.builder.boundary.AbstractBuilderView;
 import com.halaesus.kabasuji.builder.entity.*;
+import com.halaesus.kabasuji.shared.entity.AbstractLevel;
+import com.halaesus.kabasuji.shared.entity.Piece;
+import com.halaesus.kabasuji.shared.entity.PieceSquare;
+import com.halaesus.kabasuji.shared.entity.ReleaseLevel;
 
 /**
  * 
  */
-public class DragNumberToBoardMove {
+public class DragNumberToBoardMove implements MouseListener, MouseMotionListener {
 
-    /**
-     * Default constructor
-     */
-    public DragNumberToBoardMove() {
-    }
-
+   
     /**
      * 
      */
-    NumberToBoardMove move;
+    ReleaseLevel level;
 
     /**
      * 
@@ -30,29 +33,54 @@ public class DragNumberToBoardMove {
      * @param NumberToBoardMove move 
      * @param AbstractBuilderView builderView
      */
-    public DragNumberToBoardMove(NumberToBoardMove move, AbstractBuilderView builderView) {
-        // TODO implement here
+    public DragNumberToBoardMove(ReleaseLevel level, AbstractBuilderView builderView) {
+        this.level = level;
+        this.builderView = builderView;
     }
 
-    /**
-     * @param MouseEvent e
-     */
-    public void mousePressed(MouseEvent e) {
-        // TODO implement here
-    }
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    /**
-     * @param MouseEvent e
-     */
-    public void mouseDragged(MouseEvent e) {
-        // TODO implement here
-    }
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    /**
-     * @param MouseEvent e
-     */
-    public void mouseReleased(MouseEvent e) {
-        // TODO implement here
-    }
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+ 
 
 }

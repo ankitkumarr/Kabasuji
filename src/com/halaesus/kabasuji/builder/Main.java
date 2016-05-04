4bc80c367b768d310ef950894be064221c55bc48
Anthony Panetta <Anthony Panetta>
2016-05-04 02:29:42 -0400

    Documentation for builder main and application classes

--- src/com/halaesus/kabasuji/builder/Main.java
+++ src/com/halaesus/kabasuji/builder/Main.java
@@ -7,7 +7,19 @@ import com.halaesus.kabasuji.builder.boundary.Application;
 import com.halaesus.kabasuji.builder.boundary.ImageLoader;
 import com.halaesus.kabasuji.shared.entity.Model;
 
+/**
+ * Main class for the KabaSuji Builder
+ * 
+ * @author apanetta
+ */
 public class Main {
+	/**
+	 * Generates a view given a model.
+	 * 
+	 * @param m KabaSuji model to base on the view
+	 * @return Application instance of KabaSuji Builder based
+	 * on the given model
+	 */
 	public static Application setupApp(Model m) {
 		Application app = Application.instance(m);
 
@@ -22,6 +34,11 @@ public class Main {
 		return app;
 	}
 	
+	/**
+	 * Creates a new model and generates a view.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Model model = new Model();
 		final Application app = setupApp(model);
--- src/com/halaesus/kabasuji/builder/boundary/Application.java
+++ src/com/halaesus/kabasuji/builder/boundary/Application.java
@@ -10,18 +10,32 @@ import javax.swing.JPanel;
 import com.halaesus.kabasuji.shared.entity.AbstractLevel;
 import com.halaesus.kabasuji.shared.entity.Model;
 
+/**
+ * The overall view container for the KabaSuji builder.
+ *
+ */
 @SuppressWarnings("serial")
 public class Application extends JFrame {
-	
+	/** The single Application instance that is running*/
 	static Application inst;
+	
+	/** Collection to hold all of the images loaded from disc in memory */
 	HashMap<String, BufferedImage> images;
 
+	/** The View currently displayed */
     JPanel currentView;
+    /** The Model to display */
     Model masterModel;
     
+    /** The View to display the splash screen */
     SplashView splashView;
+    /** The View to display the level being built */
     AbstractBuilderView view;
     
+    /** 
+     * Generates a new Application based on the given Model
+     * @param m Model on which this view is based
+     */
     Application(Model m) {
     	// Initialize fields
 		masterModel = m;
@@ -48,6 +62,12 @@ public class Application extends JFrame {
 		showSplashScreen();
     }
     
+    /**
+     * Creates an instance based on the given model if there is not already
+     * an instance configured
+     * @param m The model to base the view on
+     * @return Reference to the instance
+     */
     public static Application instance(Model m) {
 		if (inst == null) {
 			inst = new Application(m);
@@ -57,6 +77,10 @@ public class Application extends JFrame {
 		throw new IllegalStateException("Application already configured.");
     }
     
+    /**
+     * Gets the instance if it exists
+     * @return Reference to the instance
+     */
 	public static Application instance() {
 		if (inst == null) {
 			throw new IllegalStateException("Application not yet configured.");
@@ -65,11 +89,21 @@ public class Application extends JFrame {
 		return inst;
 	}
 
+	/**
+	 * Shows the splash screen
+	 */
 	public void showSplashScreen() {
 		currentView = splashView;
 		setContentPane(currentView);
 	}
 	
+	/**
+	 * Shows a level for editing.
+	 * 
+	 * @param level The level to edit
+	 * @param type The level type
+	 * @param levelIndex The level's index
+	 */
 	public void showLevel(AbstractLevel level, String type, int levelIndex) {
 		if (type.toUpperCase().equals("PUZZLE")) {
 			view = new PuzzleBuilderView(this, level, levelIndex);
@@ -83,10 +117,19 @@ public class Application extends JFrame {
 		setContentPane(view);
 	}
 	
+	/**
+	 * Gets the requested image from the collection.
+	 * @param name Name of image
+	 * @return The requested image
+	 */
 	public BufferedImage getImage(String name) {
 		return images.get(name);
 	}
 	
+	/**
+	 * Gets the entire collection of images
+	 * @return The collection of images
+	 */
 	public HashMap<String, BufferedImage> getAllImages() {
 		return images;
 	}

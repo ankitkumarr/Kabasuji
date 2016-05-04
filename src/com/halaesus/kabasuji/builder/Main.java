988ab9963153f88578269aea140dda65a643b287
Anthony Panetta <Anthony Panetta>
2016-05-04 02:09:26 -0400

    Add test to hit 17.5% of builder boundary and 61.5% of builder's main file

--- src/com/halaesus/kabasuji/builder/Main.java
+++ src/com/halaesus/kabasuji/builder/Main.java
@@ -8,9 +8,8 @@ import com.halaesus.kabasuji.builder.boundary.ImageLoader;
 import com.halaesus.kabasuji.shared.entity.Model;
 
 public class Main {
-	public static void main(String[] args) {
-		Model model = new Model();
-		final Application app = Application.instance(model);
+	public static Application setupApp(Model m) {
+		Application app = Application.instance(m);
 
 		app.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -20,5 +19,11 @@ public class Main {
 
 		(new ImageLoader()).execute();
 		app.setVisible(true);
+		return app;
+	}
+	
+	public static void main(String[] args) {
+		Model model = new Model();
+		final Application app = setupApp(model);
 	}
 }
--- tests/com/halaesus/kabasuji/builder/TestBuilder.java
+++ tests/com/halaesus/kabasuji/builder/TestBuilder.java
@@ -1,14 +1,19 @@
 package com.halaesus.kabasuji.builder;
 
+import com.halaesus.kabasuji.builder.boundary.Application;
 import com.halaesus.kabasuji.builder.tests.BuilderTestCase;
-
-import junit.framework.TestCase;
+import com.halaesus.kabasuji.shared.entity.Model;
 
 public class TestBuilder extends BuilderTestCase {
+	Model m;
 	
+	protected void setUp() {
+		m = new Model();
+	}
 	
-	
-	
-
-
+	public void testMain() {
+		Application app = Main.setupApp(m);
+		assertTrue(app instanceof Application);
+		app.dispose();
+	}
 }

f49eff8b36d100748730158ae5f442a86cce513c
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-05-04 04:50:16 -0400

    Add JavaDoc for IMove, BoardToBoardMove, BoardToBullpenMove and WorkspaceToBoardMove classes

--- src/com/halaesus/kabasuji/player/moves/IMove.java
+++ src/com/halaesus/kabasuji/player/moves/IMove.java
@@ -4,9 +4,26 @@ import com.halaesus.kabasuji.shared.entity.AbstractLevel;
 import com.halaesus.kabasuji.shared.entity.Piece;
 import com.halaesus.kabasuji.shared.entity.PieceSquare;
 
+/**
+ * Represents a generic move functionality on the Player Side
+ * <p>
+ * @author Akshit (Axe) Soota (axe (at) wpi (dot) edu)
+ */
 public interface IMove {
 
+	/**
+	 * Verifies if the move created is a valid move
+	 * @param level
+	 * @return
+	 */
     public boolean isValid(AbstractLevel level);
+    
+    /**
+     * Performs the move on the given level
+     * @param level  the Level on which the changes should reflect
+     * @param originalPieceSquares  the Original PieceSquares of the Piece being acted upon
+     * @return
+     */
     public Piece doMove(AbstractLevel level, PieceSquare[] originalPieceSquares);
 
 }

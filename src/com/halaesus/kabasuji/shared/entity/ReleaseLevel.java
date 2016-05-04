b440a8f77f7cb10f889d290cd6bf6100e32f7d72
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-05-04 04:10:27 -0400

    Add JavaDoc to PuzzleLevel, ReleaseBoard, ReleaseLevel, ReleaseNumber, SplashModel and Workspace classes

--- src/com/halaesus/kabasuji/shared/entity/ReleaseLevel.java
+++ src/com/halaesus/kabasuji/shared/entity/ReleaseLevel.java
@@ -6,26 +6,42 @@ import com.halaesus.kabasuji.shared.memento.AbstractLevelMemento;
 import com.halaesus.kabasuji.shared.memento.ReleaseLevelMemento;
 
 /**
- * 
+ * Represents a Release Level
+ * <p>
  * @author Corey Dixon, Akshit (Axe) Soota (axe (at) wpi (dot) edu)
- *
  */
 public class ReleaseLevel extends AbstractLevel {
-  
+	
+	/** Constant signifying that the source of the drag is the ReleaseLevel NumberBar */
 	public static final int DRAG_SOURCE_NUMBERBAR = 5;
-	NumberBar numberBar;
+	
+	/** The ReleaseNumber that is being dragged */
 	ReleaseNumber numberBeingDragged;
+	
+	/** Boolean to keep track if the ReleaseNumber being dragged is over the NumberBar */
 	boolean numberOverNumberbar;
 	
+	/** NumberBar that is a part of the <code>ReleaseLevel</code> */
+	NumberBar numberBar;
+	
+	/**
+	 * Constructs a <code>ReleaseLevel</code> with a given <code>ReleaseLevelMemento</code
+	 * @param memento
+	 */
 	public ReleaseLevel(ReleaseLevelMemento memento) {
 		super(memento);
 		// Set the game type in here
 		levelType = "Release";
-		// TODO: Fix this
 		numberBar = new NumberBar();
 		numberBeingDragged = null;
 	}
 
+	/**
+	 * Public Copy Constructor for a <code>ReleaseLevel</code> which instantiates another <code>ReleaseLevel</code>
+	 * with the same attributes as the original.
+	 * <p>
+	 * @param anotherLevel  the <code>ReleaseLevel</code> to be copied over
+	 */
 	public ReleaseLevel(ReleaseLevel anotherLevel) {
 		super(anotherLevel); // Fill out Super Fields
 		// Get the NumberBar
@@ -33,20 +49,60 @@ public class ReleaseLevel extends AbstractLevel {
 		numberBeingDragged = null;
 	}
 	
+	/**
+	 * Returns the NumberBar of the <code>ReleaseLevel</code>
+	 * @return
+	 */
     public NumberBar getNumberBar(){
     	return this.numberBar;
 	}
     
+    /**
+     * Sets if the ReleaseNumber being dragged is over the NumberBar
+     * @param val
+     */
     public void setNumberOverNumberBar(boolean val) {
     	this.numberOverNumberbar = val;
     }
     
+    /**
+     * Gets if the ReleaseNumber being dragged is over the NumberBar
+     * @return
+     */
     public boolean getNumberOverNumberBar() {
     	return this.numberOverNumberbar;
     }
+	
+    /**
+     * Returns the ReleaseNumber that is being dragged over the <code>ReleaseLevel</code>
+     * @return
+     */
+	public ReleaseNumber getnumberBeingDragged(){
+	    return this.numberBeingDragged;
+	}
+	   
+	/**
+	 * Sets if a ReleaseNumber is being dragged on <code>ReleaseLevel</code>
+	 * @return
+	 */
+	public boolean isNumberDraggingActive() {
+	    return (this.numberBeingDragged!=null);
+	}
+	
+	/**
+	 * Sets the ReleaseNumber that is being dragged over the <code>ReleaseLevel</code>
+	 * @param beingDragged
+	 */
+	public void setnumberBeingDragged(ReleaseNumber beingDragged){
+	    this.numberBeingDragged = beingDragged;
+	}
         
     // Abstract Methods Filler
 
+    /**
+     * Returns the number of stars achieved by the Player on the <code>ReleaseLevel</code>
+     * @return 3 if the user has won the <code>ReleaseLevel</code>
+     */
     @Override
     public int getStarsAchieved() {
     	int setsCompleted = this.numberBar.setsFound();
@@ -59,7 +115,15 @@ public class ReleaseLevel extends AbstractLevel {
     	return setsCompleted; // The number of sets completed is equal to the number of sets found
     }
     
-    @Override
+    /**
+	 * This function is called when a Piece is dropped onto the PuzzleBoard of the <code>ReleaseLevel</code>.
+	 * <p>
+	 * The function for the <code>ReleaseLevel</code> adds any released numbers to the NumberBar and releases them from the
+	 * <code>ReleaseBoard</code>.
+	 * <p>
+	 * @param p  the Piece that was added to the Board of the <code>ReleaseLevel</code>
+	 */
+	@Override
 	public void newPieceDropped(Piece p) {
 		// Once a Piece is dropped, update the NumberBar and hence the NumberBarView
     	for(int r = 0; r < 12; r++) {
@@ -88,6 +152,10 @@ public class ReleaseLevel extends AbstractLevel {
 		checkIfRanOut();
 	}
 
+	/**
+	 * Checks if the user ran out of pieces or finished the level. The function takes necessary actions to reflect the LevelCompletion
+	 * Information.
+	 */
 	private void checkIfRanOut() {
 		// Check Pieces Left
 		int piecesLeft = 0;
@@ -104,33 +172,34 @@ public class ReleaseLevel extends AbstractLevel {
 		}
 	}
 
+	/**
+	 * This function is called when a Piece already existent on the Board was dragged to a new location on the Board of the <code>ReleaseLevel</code>
+	 * <p>
+	 * The function for the <code>ReleaseLevel</code> doesn't do anything as once a Piece is place in a <code>ReleaseLevel</code>, it cannot be moved
+	 */
 	@Override
 	public void boardPieceUpdated(PieceSquare[] oldPieceSquares, Piece newPiece) {
 		// This should not be allowed as in the ReleaseLevel once a piece is placed,
 		// it cannot be moved around.
 	}
 
+	/**
+	 * This function is called when a Piece already existent on the Board is dragged to the Player Bullpen to get rid of the Piece from the <code>ReleaseLevel</code>
+	 * <p>
+	 * The function for the <code>ReleaseLevel</code> does nothing.
+	 */
 	@Override
 	public void boardPieceRemoved(Piece p) {
 		// This should not be allowed as in the ReleaseLevel once a piece is placed, 
 		// it cannot be removed.
 	}
 
+	/**
+	 * Generates a ReleaseLevelMemento with the attributes of the <code>ReleaseLevel</code>
+	 */
 	@Override
 	public AbstractLevelMemento generateMemento() {
 		return new ReleaseLevelMemento(board.generateMemento(), bullpen.palette, this.levelIndex, this.levelType, this.levelName);
-	}
-	
-	public ReleaseNumber getnumberBeingDragged(){
-	    return this.numberBeingDragged;
-	}
-	    
-	public boolean isNumberDraggingActive() {
-	    return (this.numberBeingDragged!=null);
-	}
-	    
-	public void setnumberBeingDragged(ReleaseNumber beingDragged){
-	    this.numberBeingDragged = beingDragged;
 	}
 
 }
--- src/com/halaesus/kabasuji/shared/entity/SplashModel.java
+++ src/com/halaesus/kabasuji/shared/entity/SplashModel.java
@@ -5,10 +5,19 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * A Model class that holds the Splash Image
+ * <p>
+ * @author Akshit (Axe) Soota (axe (at) wpi (dot) edu)
+ */
 public class SplashModel {
 
+	/** Image which holds the splash image */
     BufferedImage splashImage;
     
+    /**
+     * Generates an instance of the SplashModel class and loads up the Splash Image
+     */
     public SplashModel() {
     	try {
 			splashImage = ImageIO.read(SplashModel.class.getResourceAsStream("/resources/splash.jpg"));
@@ -17,6 +26,10 @@ public class SplashModel {
 		}
     }
 
+    /**
+     * Returns the Splash Image
+     * @return
+     */
 	public BufferedImage getSplashImage() {
 		return splashImage;
 	}

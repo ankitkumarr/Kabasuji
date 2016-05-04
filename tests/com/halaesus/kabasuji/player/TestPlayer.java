a7da8b507fc8825e13d9ed7a40d1d93449bca1ba
Ankit Kumar <kumar.ankit55@gmail.com>
2016-05-04 03:08:32 -0400

    More Coverage for controllers in Player

--- tests/com/halaesus/kabasuji/player/TestPlayer.java
+++ tests/com/halaesus/kabasuji/player/TestPlayer.java
@@ -2,14 +2,21 @@ package com.halaesus.kabasuji.player;
 
 import com.halaesus.kabasuji.player.tests.PlayerTestCase;
 
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
+
+import com.halaesus.kabasuji.player.boundary.AbstractLevelView;
 import com.halaesus.kabasuji.player.boundary.Application;
 import com.halaesus.kabasuji.player.boundary.LightningLevelView;
 import com.halaesus.kabasuji.player.boundary.PuzzleLevelView;
+import com.halaesus.kabasuji.player.controller.DragPieceFromBoard;
+import com.halaesus.kabasuji.player.controller.DragPieceFromWorkspaceToBoard;
 import com.halaesus.kabasuji.player.entity.LevelList;
 import com.halaesus.kabasuji.player.entity.PlayerProgress;
+import com.halaesus.kabasuji.shared.entity.AbstractBoard;
 import com.halaesus.kabasuji.shared.entity.AbstractLevel;
 import com.halaesus.kabasuji.shared.entity.Model;
 import com.halaesus.kabasuji.shared.entity.Piece;
@@ -63,6 +70,16 @@ public class TestPlayer extends PlayerTestCase {
 		app.showLevelSelector();
 	}
 	
+	protected AbstractLevel setupBoardPuzzle(AbstractLevel l) {
+		for(int r = 0; r < 12; r++)
+			for(int c = 0; c < 12; c++)
+				l.getBoard().getSquares()[r][c].setActive(true);
+		return l;
+	}
+	
+	protected void addPiece(AbstractLevel l) {
+		l.getBoard().addPiece(PieceGenerator.pieces[0]);
+	}
 	protected void tearDown() {
 		app.dispose();
 		// delete the level files we created for testing
@@ -79,11 +96,81 @@ public class TestPlayer extends PlayerTestCase {
 			}
 		}
 	}
+	
+	public MouseEvent createPressed (AbstractLevelView game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_PRESSED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	public MouseEvent createDragged (AbstractLevelView game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_DRAGGED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
+	public MouseEvent createRightClick (AbstractLevelView game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_PRESSED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
+	public MouseEvent createReleased (AbstractLevelView game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_RELEASED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
+	public MouseEvent createClicked (AbstractLevelView game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_CLICKED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
+	public MouseEvent createDoubleClicked (AbstractLevelView game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_CLICKED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+
 
 	public void testPlayUnlockedPuzzleLevel(){
 		// test opening the first level to play
+		
 		AbstractLevel level = masterModel.getLevelSelectorModel().getLevelList().loadLevel(0);
+		level = setupBoardPuzzle(level);
+		addPiece(level);
+		AbstractLevelView newView = new PuzzleLevelView(app, (PuzzleLevel) level);
+		
+		MouseEvent me = (createPressed(newView, newView.getBullpenWorkspacePieceRectangle(2, 1).x, newView.getBullpenWorkspacePieceRectangle(2, 1).y));
+		MouseEvent me1 = (createDragged(newView, newView.getBoardPieceRectangle(6, 6).x, newView.getBoardPieceRectangle(6, 6).y));
+		MouseEvent me2 = (createReleased(newView, newView.getBoardPieceRectangle(6, 6).x, newView.getBoardPieceRectangle(6, 6).y));
+		MouseEvent me3 = (createPressed(newView, newView.getBoardPieceRectangle(0, 0).x, newView.getBoardPieceRectangle(0, 0).y));
+		MouseEvent me4 = (createReleased(newView, newView.getBullpenWorkspacePieceRectangle(2, 1).x, newView.getBullpenWorkspacePieceRectangle(2, 1).y));
+		
+		level.getLevelBullpen().getWorkspace().addPiece(PieceGenerator.pieces[33]);
+		DragPieceFromWorkspaceToBoard t1 = new DragPieceFromWorkspaceToBoard(level, newView);
+		DragPieceFromBoard t2 = new DragPieceFromBoard(level, newView);
+		t1.mousePressed(me);
+		t1.mouseDragged(me1);
+		t2.mouseReleased(me2);
+		t1.mouseReleased(me2);
 		app.showLevel(level);
+		
+		t2.mousePressed(me3);
+		t2.mouseDragged(me1);
+		t2.mouseReleased(me2);
+		t2.mouseReleased(me4);
 		assertTrue(true);
 	}
 	
@@ -118,6 +205,12 @@ public class TestPlayer extends PlayerTestCase {
 		piece.noSquareRight(square);
 		piece.pushTopLeft();
 		assertTrue(true);	
+	}
+	
+	public void testMain() {
+		Main.main(null);
+		assertTrue(true);	
+
 	}
 	
 	

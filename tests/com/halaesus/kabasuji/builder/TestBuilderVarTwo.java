b95cad9fff5b83e70380f75817641c20df6a7048
Ankit Kumar <kumar.ankit55@gmail.com>
2016-05-04 06:07:01 -0400

    Coverage increased to 61.3%

--- tests/com/halaesus/kabasuji/builder/TestBuilderVarTwo.java
+++ tests/com/halaesus/kabasuji/builder/TestBuilderVarTwo.java
@@ -1,16 +1,24 @@
 package com.halaesus.kabasuji.builder;
 
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
+
+import com.halaesus.kabasuji.builder.boundary.AbstractBuilderView;
 import com.halaesus.kabasuji.builder.boundary.Application;
 import com.halaesus.kabasuji.builder.boundary.ImageLoader;
 import com.halaesus.kabasuji.builder.boundary.LevelManagerDialog;
+import com.halaesus.kabasuji.builder.boundary.PuzzleBuilderView;
+import com.halaesus.kabasuji.builder.controller.DragPieceFromBoard;
+import com.halaesus.kabasuji.builder.controller.DragPieceFromWorkspace;
 import com.halaesus.kabasuji.builder.tests.BuilderTestCase;
 import com.halaesus.kabasuji.player.entity.LevelList;
 import com.halaesus.kabasuji.shared.entity.AbstractLevel;
 import com.halaesus.kabasuji.shared.entity.Model;
 import com.halaesus.kabasuji.shared.entity.PuzzleLevel;
+import com.halaesus.kabasuji.utils.PieceGenerator;
 
 public class TestBuilderVarTwo extends BuilderTestCase {
 	
@@ -65,6 +73,9 @@ public class TestBuilderVarTwo extends BuilderTestCase {
 		}
 	}
 	
+	protected void addPiece(AbstractLevel l) {
+		l.getBoard().addPiece(PieceGenerator.pieces[0]);
+	}
 	
 	public void testSomething() {
 		assertTrue(true);
@@ -85,6 +96,45 @@ public class TestBuilderVarTwo extends BuilderTestCase {
 	public void testLoadReleaseLevel(){
 		AbstractLevel releaseLevel =  levelList.loadLevel(2);
 		app.showLevel(releaseLevel,"RELEASE",	2);
+		assertTrue(true);
+	}
+	
+	public MouseEvent createPressed (AbstractBuilderView game, int dx, int dy, int click) {
+		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_PRESSED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false, click);
+		return me;
+	}
+	
+	public void testSomeMovePuzzleLevel() {
+		
+		AbstractLevel level = model.getLevelSelectorModel().getLevelList().loadLevel(2);
+		addPiece(level);
+		AbstractBuilderView newView = new PuzzleBuilderView(app, (PuzzleLevel) level, 0);
+		MouseEvent me = (createPressed(newView, newView.getBullpenWorkspacePieceRectangle(2, 1).x, newView.getBullpenWorkspacePieceRectangle(2, 1).y));
+		MouseEvent me1 = (createDragged(newView, newView.getBoardPieceRectangle(6, 6).x, newView.getBoardPieceRectangle(6, 6).y));
+		MouseEvent me2 = (createReleased(newView, newView.getBoardPieceRectangle(6, 6).x, newView.getBoardPieceRectangle(6, 6).y));
+		MouseEvent me3 = (createPressed(newView, newView.getBoardPieceRectangle(0, 0).x, newView.getBoardPieceRectangle(0, 0).y));
+		MouseEvent me4 = (createReleased(newView, newView.getBullpenWorkspacePieceRectangle(2, 1).x, newView.getBullpenWorkspacePieceRectangle(2, 1).y));
+		
+		MouseEvent me5 = (createPressed(newView, newView.getBoardPieceRectangle(0, 0).x, newView.getBoardPieceRectangle(0, 0).y, 1));
+
+		DragPieceFromBoard move1 = new DragPieceFromBoard(level, newView);
+		move1.mousePressed(me5);
+		move1.mouseDragged(me1);
+		move1.mouseReleased(me2);
+		
+		DragPieceFromBoard move3 = new DragPieceFromBoard(level, newView);
+		move1.mousePressed(me5);
+		move1.mouseDragged(me1);
+		move1.mouseReleased(me4);
+
+		
+		level.getLevelBullpen().getWorkspace().addPiece(PieceGenerator.pieces[33]);
+		DragPieceFromWorkspace move2 = new DragPieceFromWorkspace(level, newView);
+		move2.mousePressed(me);
+		move2.mouseDragged(me1);
+		move2.mouseReleased(me2);
 		assertTrue(true);
 	}
 	

5eadf2c66beb44d876234c007efcbb3b4a394006
Anthony Panetta <Anthony Panetta>
2016-05-04 02:49:26 -0400

    Finished documentation for builder boundary

--- src/com/halaesus/kabasuji/builder/boundary/SplashView.java
+++ src/com/halaesus/kabasuji/builder/boundary/SplashView.java
@@ -8,17 +8,28 @@ import javax.swing.JPanel;
 
 import com.halaesus.kabasuji.shared.entity.SplashModel;
 
+/**
+ * View to display the Splash Screen for the KabaSuji Builder
+ *
+ */
 @SuppressWarnings("serial")
 public class SplashView extends JPanel {
-
+	/** The associated model for this view */
 	SplashModel splashModel;
 
+	/**
+	 * Generates a SplashView based on the data in the given SplashModel
+	 * @param splashModel model to base this view on
+	 */
 	public SplashView(SplashModel splashModel) {
 		this.splashModel = splashModel;
 
 		setPreferredSize(new Dimension(1280, 720));
 	}
 
+	/**
+	 * Paint this component by drawing the splash screen image
+	 */
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);

77eaf458a4f6295a4f603da77e590689fe66bf05
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-05-04 01:36:41 -0400

    Add JavaDoc for LightningLevel and Model classes

--- src/com/halaesus/kabasuji/shared/entity/LightningLevel.java
+++ src/com/halaesus/kabasuji/shared/entity/LightningLevel.java
@@ -6,16 +6,25 @@ import com.halaesus.kabasuji.shared.memento.AbstractLevelMemento;
 import com.halaesus.kabasuji.shared.memento.LightningLevelMemento;
 
 /**
- * 
+ * Represents a Lightning Level
+ * <p>
  * @author Corey Dixon, Akshit (Axe) Soota (axe (at) wpi (dot) edu)
- *
  */
 public class LightningLevel extends AbstractLevel {
 
+	/** The number of elapsed seconds in the <code>LightningLevel</code> */
 	int elapsedTime;
+	
+	/** The maximum time the user can play for in the <code>LightningLevel</code> */
 	int maxTime;
+	
+	/** The number of random Pieces available for the Player to play at anytime during a <code>LightningLevel</code> */
 	int numRandomPieces;
 
+	/**
+	 * Constructs a <code>LightningLevel</code> with a given <code>LightningLevelMemento</code>
+	 * @param memento
+	 */
 	public LightningLevel(LightningLevelMemento memento) {
 		super(memento); // Let the super do its stuff with the memento
 		// Fill out some fields
@@ -28,6 +37,12 @@ public class LightningLevel extends AbstractLevel {
 		updateBullpenPieces();
 	}
 
+	/**
+	 * Public Copy Constructor for a <code>LightningLevel</code> which instantiates another <code>LightningLevel</code> with the
+	 * same attributes as the original.
+	 * <p>
+	 * @param anotherLevel  The <code>LightningLevel</code> to be copied over
+	 */
 	public LightningLevel(LightningLevel anotherLevel) {
 		super(anotherLevel); // Fill out Super Fields
 		// Fill out our fields now
@@ -36,35 +51,66 @@ public class LightningLevel extends AbstractLevel {
 		this.maxTime = anotherLevel.maxTime;
 	}
 
+	/**
+	 * Returns the time left for the player to play the <code>LightningLevel</code>
+	 * @return
+	 */
 	public int getTimeLeft() {
 		return (maxTime - elapsedTime);
 	}
 
+	/**
+	 * Increments the time used up by the Player on the <code>LightningLevel</code> by 1 second
+	 */
 	public void incrementElapsedTime() {
 		elapsedTime++;
 	}
 
+	/**
+	 * Sets the elapsed time by the Player on the <code>LightningLevel</code> to 0 seconds
+	 */
 	public void resetElapsedTime() {
 		elapsedTime = 0;
 	}
+	
+	/**
+	 * Returns the maximum allowed time for the Player to play the <code>LightningLevel</code>
+	 * @return
+	 */
 	public int getMaxTime() {
 		return this.maxTime;
 	}
 	
+	/**
+	 * Returns the number of random Pieces that will be visible to a Player playing the <code>LightningLevel</code> at a given point
+	 * @return
+	 */
 	public int getNumRandomPieces() {
 		return this.numRandomPieces;
 	}
 	
+	/**
+	 * Sets the maximum time given to the Player to play the <code>LightningLevel</code>
+	 * @param num
+	 */
 	public void setMaxTime(int num) {
 		this.maxTime = num;
 	}
 	
+	/**
+	 * Sets the number of random Pieces that will be visible to a Player playing the <code>LightningLevel</code>
+	 * @param randomMoves
+	 */
 	public void setRandomMoves(int randomMoves) {
 		this.numRandomPieces = randomMoves;
 	}
 
 	// Abstract Methods Filler
 
+	/**
+	 * Returns the number of stars achieved by the Player on the <code>LightningLevel</code>
+	 * @return 3 if the user has won the <code>LightningLevel</code>
+	 */
 	@Override
 	public int getStarsAchieved() {
 		int activeSquares = 0;
@@ -96,6 +142,14 @@ public class LightningLevel extends AbstractLevel {
 		}
 	}
 
+	/**
+	 * This function is called when a Piece is dropped onto the LightningBoard of the <code>LightningLevel</code>.
+	 * <p>
+	 * The function for the LightningLevel removes the Piece from the LightningBoard as there are not suppose to be any pieces on
+	 * a LightningBoard.
+	 * <p>
+	 * @param p  the Piece that was added to the Board of the <code>LightningLevel</code>
+	 */
 	@Override
 	public void newPieceDropped(Piece p) {
 		// Remove the Piece from the Board
@@ -104,6 +158,9 @@ public class LightningLevel extends AbstractLevel {
 		updateBullpenPieces();
 	}
 
+	/**
+	 * Generates random pieces on the board to be added to the Player Palette
+	 */
 	private void updateBullpenPieces() {
 		// STEP: Count how many are we short of
 		int hexominoTotalCount = 0;
@@ -118,18 +175,31 @@ public class LightningLevel extends AbstractLevel {
 		}
 	}
 
+	/**
+	 * This function is called when a Piece already existent on the Board was dragged to a new location on the Board of the <code>LightningLevel</code>
+	 * <p>
+	 * The function for the LightningLevel does nothing as this is not a valid move for a <code>LightningLevel</code>
+	 */
 	@Override
 	public void boardPieceUpdated(PieceSquare[] oldPieceSquares, Piece newPiece) {
 		// This should never be called on the LightningLevel as once a Piece is
 		// dropped, it gets soaked by the board and no longer exists.
 	}
 
+	/**
+	 * This function is called when a Piece already existent on the Board is dragged to the Player Bullpen to get rid of the Piece from the <code>LightningLevel</code>
+	 * <p>
+	 * The function for the LightningLevel does nothing as this is also not an allowed move for <code>LightningLevel</code>
+	 */
 	@Override
 	public void boardPieceRemoved(Piece p) {
 		// This should never be called on the LightningLevel either because once a
 		// Piece is dropped, it gets soaked by the board and no longer exists to be removed.
 	}
 
+	/**
+	 * Generates an LightningLevelMemento with the attributes of the <code>LightningLevel</code>
+	 */
 	@Override
 	public AbstractLevelMemento generateMemento() {
 		return new LightningLevelMemento(board.generateMemento(), bullpen.palette, this.levelIndex, maxTime, numRandomPieces, this.levelType, this.levelName);
--- src/com/halaesus/kabasuji/shared/entity/Model.java
+++ src/com/halaesus/kabasuji/shared/entity/Model.java
@@ -3,32 +3,61 @@ package com.halaesus.kabasuji.shared.entity;
 import com.halaesus.kabasuji.player.entity.LevelSelector;
 import com.halaesus.kabasuji.player.entity.PlayerProgress;
 
+/**
+ * Represents the main Model class of the KabaSuji app.
+ * <p>
+ * @author Akshit (Axe) Soota (axe (at) wpi (dot) edu)
+ */
 public class Model {
 	
+	/** The Splash Screen Model */
 	SplashModel splashModel;
+	
+	/** The Player Progress Data Structure */
 	PlayerProgress playerProgress;
+	
+	/** Level Selector Model to help the Player choose the Levels to play */
 	LevelSelector levelSelector;
-		
+	
+	/**
+	 * Instantiates a Model class with a given PlayerProgress
+	 * @param playerProgress the PlayerProgress to construct the Model class with
+	 */
 	public Model(PlayerProgress playerProgress) {
 		splashModel = new SplashModel();
 		this.playerProgress = playerProgress;
 		levelSelector = new LevelSelector(playerProgress);
 	}
 	
+	/**
+	 * Constructs a Model class
+	 */
 	public Model() {
 		splashModel = new SplashModel();
 		this.playerProgress = new PlayerProgress();
 		levelSelector = new LevelSelector(playerProgress);
 	}
 
+	/**
+	 * Returns the Splash Screen Model
+	 * @return
+	 */
 	public SplashModel getSplashModel() {
 		return splashModel;
 	}
 	
+	/**
+	 * Returns the Level Selector Screen Model
+	 * @return
+	 */
 	public LevelSelector getLevelSelectorModel() {
 		return levelSelector;
 	}
 
+	/**
+	 * Returns the Model that tracks a Player's Progress
+	 * @return
+	 */
 	public PlayerProgress getPlayerProgress() {
 		return playerProgress;
 	}

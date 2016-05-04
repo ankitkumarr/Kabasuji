8de4398c7651dad9a423c382fa4f6fccce36540b
Brianjkd <brianjkd@gmail.com>
2016-05-04 03:27:20 -0400

    Add possible setup for Builder. Currently fails to succesfuly load the created levels

--- tests/com/halaesus/kabasuji/player/TestPlayer.java
+++ tests/com/halaesus/kabasuji/player/TestPlayer.java
@@ -97,43 +97,6 @@ public class TestPlayer extends PlayerTestCase {
 		}
 	}
 	
-	public MouseEvent createPressed (AbstractLevelView game, int dx, int dy) {
-		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_PRESSED, 
-				System.currentTimeMillis(), 0, 
-				dx, dy, 0, false);
-		return me;
-	}
-	
-	public MouseEvent createDragged (AbstractLevelView game, int dx, int dy) {
-		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_DRAGGED, 
-				System.currentTimeMillis(), 0, 
-				dx, dy, 0, false);
-		return me;
-	}
-	
-	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
-	public MouseEvent createRightClick (AbstractLevelView game, int dx, int dy) {
-		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_PRESSED, 
-				System.currentTimeMillis(), 0, 
-				dx, dy, 0, false);
-		return me;
-	}
-	
-	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
-	public MouseEvent createReleased (AbstractLevelView game, int dx, int dy) {
-		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_RELEASED, 
-				System.currentTimeMillis(), 0, 
-				dx, dy, 0, false);
-		return me;
-	}
-	
-	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
-	public MouseEvent createClicked (AbstractLevelView game, int dx, int dy) {
-		MouseEvent me = new MouseEvent(new JLabel(), MouseEvent.MOUSE_CLICKED, 
-				System.currentTimeMillis(), 0, 
-				dx, dy, 0, false);
-		return me;
-	}
 	
 	/** (dx,dy) are offsets into the widget space. Feel Free to Use as Is. */
 	public MouseEvent createDoubleClicked (AbstractLevelView game, int dx, int dy) {
